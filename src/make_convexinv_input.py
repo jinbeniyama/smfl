bba58c6d98e4811a5680bf1c2ed380ecdb425c0b
Jin Beniyama <beniyama@ioa.s.u-tokyo.ac.jp>
2025-04-13 15:00:30 +0200

    Fix typo

--- src/make_convexinv_input.py
+++ src/make_convexinv_input.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
         lam = [args.lam]
         beta = [args.beta]
     if args.N_golden:
-        lam, beta = golden_spiral_spherical(args.N_golden)
+        lam, beta = golden_spiral_G10(args.N_golden)
     else:
         lam = np.linspace(0, 360, args.Nlam)
         beta = np.linspace(-90, 90, args.Nbeta)
