3038dfdde5e51adc044bb79382f2b1e27432e654
Jin Beniyama <beniyama@ioa.s.u-tokyo.ac.jp>
2025-04-23 11:50:08 +0200

    Fix typo

--- src/plot_lcs_with_model.py
+++ src/plot_lcs_with_model.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt  
 
-from smfl import figure4lc, plot_lc_wmodel, plt_plc_wmodel
+from smfl import figure4lc, plot_lc_wmodel, plot_plc_wmodel
 
 
 if __name__ == "__main__":
